4ee47730ad9bbcd5439ced7dca26b41c51a2bd74
pumpash <92608913+pumpash@users.noreply.github.com>
2023-09-22 17:21:47 +0300

    CalcTimurlan.java

--- CalcTimurlan.java
+++ CalcTimurlan.java
@@ -2,78 +2,67 @@ import java.util.Scanner;
 
 public class CalcTimurlan {
     public static void main(String[] args) {
-        //2+3
-        //X+V=XV
-        Convert converter = new Convert();
-        String[] actions = {"+", "-", "/", "*"};
-        String[] regexActions = {"\\+", "-", "/", "\\*"};
         Scanner scn = new Scanner(System.in);
         System.out.print("Введите выражение: ");
         String exp = scn.nextLine();
-        //Определяем арифметическое действие:
-        int actionIndex=-1;
-        for (int i = 0; i < actions.length; i++) {
-            if(exp.contains(actions[i])){
-                actionIndex = i;
-                break;
-            }
+
+        try {
+            String result = calc(exp);
+            System.out.println(result);
+        } catch (ArithmeticException e) {
+            System.out.println(e.getMessage());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
         }
-        //Если не нашли арифметического действия, завершаем программу
-        if(actionIndex==-1){
-            System.out.println("Некорректное выражение");
-            return;
+    }
+
+    public static String calc(String input) {
+        Convert converter = new Convert();
+        String[] actions = {"+", "-", "*", "/"};
+        String[] regexActions = {"\\+", "-", "\\*", "/"};
+        String[] data = input.split(regexActions[0]);
+
+        if (data.length != 2) {
+            throw new IllegalArgumentException("Некорректное выражение - должно быть два операнда");
         }
-        //Делим строчку по найденному арифметическому знаку
 
+        boolean isRoman = converter.isRoman(data[0]) && converter.isRoman(data[1]);
 
-        String[] data = exp.split(regexActions[actionIndex]);
-        //Определяем, находятся ли числа в одном формате (оба римские или оба арабские)
-        if(converter.isRoman(data[0]) == converter.isRoman(data[1])){
-            int a,b;
-            //Определяем, римские ли это числа
-            boolean isRoman = converter.isRoman(data[0]);
-            if(isRoman){
-                //если римские, то конвертируем их в арабские
-                //X+V
-                //x-10
-                //v - 5
-                a = converter.romanToInt(data[0]);
-                b = converter.romanToInt(data[1]);
-
-            }else{
-                //если арабские, конвертируем их из строки в число
+        int a, b;
+        if (isRoman) {
+            a = converter.romanToInt(data[0]);
+            b = converter.romanToInt(data[1]);
+        } else {
+            try {
                 a = Integer.parseInt(data[0]);
                 b = Integer.parseInt(data[1]);
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException("Некорректные числа");
             }
-            //выполняем с числами арифметическое действие
-            int result;
-            switch (actions[actionIndex]){
-                case "+":
-                    result = a+b;
-                    break;
-                case "-":
-                    result = a-b;
-                    break;
-                case "*":
-                    result = a*b;
-                    break;
-                default:
-                    result = a/b;
-                    break;
-            }
-            //15->XV
-            if(isRoman){
-                //если числа были римские, возвращаем результат в римском числе
-                System.out.println(converter.intToRoman(result));
-            }
-            else{
-                //если числа были арабские, возвращаем результат в арабском числе
-                System.out.println(result);
-            }
-        }else{
-            System.out.println("Числа должны быть в одном формате");
         }
 
+        if (a < 1 || a > 10 || b < 1 || b > 10) {
+            throw new IllegalArgumentException("Числа должны быть от 1 до 10");
+        }
 
+        int result;
+        switch (input.charAt(data[0].length())) {
+            case '+':
+                result = a + b;
+                break;
+            case '-':
+                result = a - b;
+                break;
+            case '*':
+                result = a * b;
+                break;
+            case '/':
+                result = a / b;
+                break;
+            default:
+                throw new IllegalArgumentException("Некорректная арифметическая операция");
+        }
+
+        return isRoman ? converter.intToRoman(result) : String.valueOf(result);
     }
 }
