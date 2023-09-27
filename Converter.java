2256741d672bcc4b39549ea20e70202322099dbc
pumpash <92608913+pumpash@users.noreply.github.com>
2023-09-27 21:45:43 +0300

    Converter.java

--- Converter.java
+++ Converter.java
@@ -8,19 +8,9 @@ public class Converter {
         romanKeyMap.put('I', 1);
         romanKeyMap.put('V', 5);
         romanKeyMap.put('X', 10);
-        romanKeyMap.put('L', 50);
-        romanKeyMap.put('C', 100);
-        romanKeyMap.put('D', 500);
-        romanKeyMap.put('M', 1000);
+        
 
-        arabianKeyMap.put(1000, "M");
-        arabianKeyMap.put(900, "CM");
-        arabianKeyMap.put(500, "D");
-        arabianKeyMap.put(400, "CD");
-        arabianKeyMap.put(100, "C");
-        arabianKeyMap.put(90, "XC");
-        arabianKeyMap.put(50, "L");
-        arabianKeyMap.put(40, "XL");
+    
         arabianKeyMap.put(10, "X");
         arabianKeyMap.put(9, "IX");
         arabianKeyMap.put(5, "V");
