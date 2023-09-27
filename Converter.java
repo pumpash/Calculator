9df2c0c52d80c55f475079700d4c96e799fec3c7
pumpash <92608913+pumpash@users.noreply.github.com>
2023-09-27 21:51:22 +0300

    Converter.java

--- Converter.java
+++ Converter.java
@@ -6,7 +6,14 @@ public class Converter {
 
     public Converter() {
         romanKeyMap.put('I', 1);
+        romanKeyMap.put('II', 2);
+        romanKeyMap.put('III', 3);
+        romanKeyMap.put('IV', 4);
         romanKeyMap.put('V', 5);
+        romanKeyMap.put('VI', 6);
+        romanKeyMap.put('VII', 7);
+        romanKeyMap.put('VIII', 8);
+        romanKeyMap.put('IX', 9);
         romanKeyMap.put('X', 10);
       
         arabianKeyMap.put(10, "X");
